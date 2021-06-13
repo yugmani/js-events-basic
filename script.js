e9351c54eede50de749908a7284d9031839a2002
yugmani <yugmani@yahoo.com>
2021-06-12 19:19:27 -0700

    fix bugs

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const mouseoverEl = document.getElementById('mouse-over');
 const keydownEl = document.getElementById('keydown');
+const pageLoadEl = document.querySelector('.event1');
+const containerEl = document.querySelector('.container');
 
 function pageload() {
-  alert('Page loaded Successfully!');
+  // alert('Page loaded Successfully!');
+  document.body.backgroundColor = 'whitesmoke';
+  containerEl.style.display = 'none';
+
+  setTimeout(function() {
+    pageLoadEl.style.display = 'none';
+    containerEl.style.display = 'block';
+  }, 2000);
   //refersh the web page and check it
 }
 
